d196568675571ef1a339623c4dbb61091ff7e3ec
D-oxys <sanuzumaki12@gmail.com>
2023-04-27 16:00:03 +0700

    update 6 hapus love + link header

--- pages/Component/header.tsx
+++ pages/Component/header.tsx
@@ -7,7 +7,7 @@ function header() {
       <header>
         <nav className=" bg-transparent px-4 py-2 lg:px-6">
           <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
-            <a href="https://flowbite.com" className="flex items-center">
+            <a href="" className="flex items-center">
               <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
               <span className="bg-te self-center whitespace-nowrap text-xl font-semibold text-slate-600">Ichsan</span>
             </a>
